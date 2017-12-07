96b5eca74eaff47231841673b62291b8e5df5bed
msuwala <34213290+msuwala@users.noreply.github.com>
2017-12-07 10:03:55 +0100

    Update day6.py
    Optimized by switching to dict instead of list.
    Runtime cut from ~1,17 to ~0,07

--- solutions/day6.py
+++ solutions/day6.py
@@ -1,22 +1,21 @@
-def solve(infile, twist=False):
-    reg = []
-    seen = []
-    for x in infile.readline().split():
-        reg.append(int(x))
+def solve(reg, twist=False):
+    seen = dict()
     c = 0
-    while seen.count(','.join(map(str, reg))) == 0:
-        seen.append(','.join(map(str, reg)))
-        i = reg.index(max(reg))
-        s = reg[i]
-        reg[i] = 0
+    while seen.get(reg) is None:
+        seen.update({reg: c})
+        l = list(reg)
+        i = l.index(max(l))
+        s = l[i]
+        l[i] = 0
         while s > 0:
-            reg[(i + 1) % len(reg)] += 1
+            l[(i + 1) % 16] += 1
             i += 1
             s += -1
         c += 1
-    return c if not twist else c - seen.index(','.join(map(str, reg)))
+        reg = tuple(l)
+    return c if not twist else c - seen.get(reg)
 
 with open("../resources/day6") as infile:
-    print(solve(infile))
-    infile.seek(0)
-    print(solve(infile, True))
+    data = tuple(int(x) for x in infile.readline().split())
+    print(solve(data))
+    print(solve(data, True))
