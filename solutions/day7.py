20786b787a9341515deb35bb517b1436b006cdd2
msuwala <xswistaqx@gmail.com>
2017-12-10 12:07:59 +0100

    Testing ability to push to github.

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,2 +1,33 @@
-print('Test')
-print("test different account")
+import re
+
+# from part 1 solved in browser using "find"
+root = "gynfwly"
+
+
+def solve():
+	with open("../resources/day7") as infile:
+		tree = dict()
+		for line in infile:
+			match = re.match(r"^([a-z]*)\s\(([0-9]*)\)(\s->\s)?(.*)", line)
+			tree.update({match.group(1): (int(match.group(2)), match.group(4).split(", "))})
+	step(tree, root)
+
+
+def step(tree: dict, node: str):
+	children = tree.get(node)[1]
+	if children == [''] or :
+
+
+
+def weight(tree: dict, node: str):
+	children = tree.get(node)[1]
+	if children == ['']:
+		return tree.get(node)[0]
+	else:
+		s = 0
+		for child in children:
+			s += weight(tree, child)
+		return tree.get(node)[0] + s
+
+if __name__ == "__main__":
+	solve()
